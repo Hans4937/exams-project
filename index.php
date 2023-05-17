62317676b555d0b5b9fe19bf173e89303afc42c6
tdkr49380 <tdkr49380@edu.ucl.dk>
2023-05-17 20:54:51 +0200

    Sticky navbar

--- index.php
+++ index.php
@@ -10,62 +10,49 @@
   <script src="./scripts.js"></script>
   <link rel="stylesheet" href="./styles.css"></link>
 
-  
-
 </head>
 
 
 <body>
-  <!--Header-->
+  <!-- Header -->
   <div class="Header">
-    <!--Div inside header-->
+    <!-- Div inside header -->
     <div class="InnerHeader">
-     <!--Logo in InnerHeader--> 
+     <!-- Logo in InnerHeader --> 
       <div class="LogoContainer">
         <h1>Nordic <span>Greens</span></h1>
       </div>
-      <!-- navigation inside InnerHeader-->
-        <ul class="Navigation">
-          <a href="MinisLegeRUm"><li>Produkter</li></a>
-          <a href="#"><li>Brands</li></a>  
-          <a href="#"><li>Madsspil</li></a>
-          <a href="#"><li>Miljø</li></a>
-          <a href="#"><li>Om os</li></a>
-          <a href="#"><li>Opskrifter</li></a>
-          <a href="#"><li>Jobs</li></a>
-          <a href="#"><li>Erhverv</li></a>
-          <a href="#"><li>Kontakt os</li></a>
-        </ul>
-
-
-
-
-
+      <!-- Navigation inside InnerHeader -->
+      <ul class="Navigation">
+        <a href="MinisLegeRUm"><li>Produkter</li></a>
+        <a href="#"><li>Brands</li></a>  
+        <a href="#"><li>Madsspil</li></a>
+        <a href="#"><li>Miljø</li></a>
+        <a href="#"><li>Om os</li></a>
+        <a href="#"><li>Opskrifter</li></a>
+        <a href="#"><li>Jobs</li></a>
+        <a href="#"><li>Erhverv</li></a>
+        <a href="#"><li>Kontakt os</li></a>
+      </ul>
+    </div>
   </div>
 
-  <div class=Container>
-    
-  </div>
-
-  <div class=Container0>
-    
+  <div class="Container">
+    <div class="text"></div>
+    <div class="img"></div>
   </div>
 
 
+  <div class="Container0"></div>
 
   <footer>
     Hello
   </footer>
 
-  <!-- starting php code -->
+  <!-- Starting PHP code -->
   <?php
-
 
   ?>
 
-
-
 </body>
-
-
 </html>
