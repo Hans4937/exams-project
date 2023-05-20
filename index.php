d9e4e3589bfb29dc2ebee18f1aa36c07328d28ba
tdkr49380 <tdkr49380@edu.ucl.dk>
2023-05-20 14:38:38 +0200

    Jojo

--- index.php
+++ index.php
@@ -4,11 +4,12 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Nordic Greens</title>
 
   <!-- linking script and stylesheet -->
   <script src="./scripts.js"></script>
   <link rel="stylesheet" href="./styles.css"></link>
+
 
 </head>
 
@@ -20,30 +21,42 @@
     <div class="InnerHeader">
      <!-- Logo in InnerHeader --> 
       <div class="LogoContainer">
-        <h1>Nordic <span>Greens</span></h1>
+        <a href="index.php"><h1>Nordic <span>Greens</span></h1></a>
       </div>
       <!-- Navigation inside InnerHeader -->
       <ul class="Navigation">
-        <a href="MinisLegeRUm"><li>Produkter</li></a>
-        <a href="#"><li>Brands</li></a>  
-        <a href="#"><li>Madsspil</li></a>
-        <a href="#"><li>Miljø</li></a>
-        <a href="#"><li>Om os</li></a>
-        <a href="#"><li>Opskrifter</li></a>
-        <a href="#"><li>Jobs</li></a>
-        <a href="#"><li>Erhverv</li></a>
-        <a href="#"><li>Kontakt os</li></a>
+        <a href="produkter.php"><li>Produkter</li></a>
+        <a href="brands.php"><li>Brands</li></a>  
+        <a href="madspild.php"><li>Madspild</li></a>
+        <a href="miljoeCSR.php"><li>Miljø & CSR</li></a>
+        <a href="omOs.php"><li>Om os</li></a>
+        <a href="opskrifter.php"><li>Opskrifter</li></a>
+        <a href="jobs.php"><li>Jobs</li></a>
+        <a href="login.php"><li>Erhverv</li></a>
+        <button class="KontaktOs" href="kontaktOs.php"><li>Kontakt os</li></button>
       </ul>
     </div>
   </div>
 
   <div class="Container">
-    <div class="text"></div>
-    <div class="img"></div>
+  <div class="text">
+    <h1>Danmarks mest forkælede
+tomater, agurker og
+peberfrugter <span>Vi elsker naturligvis tomater, agurker og peberfrugter. Det gør vi, fordi de ganske simpelt smager fantastisk, men også for deres gode, sunde egenskaber. Selv den mindste lille tomat er en sand bombe af gode og vigtige vitaminer og mineraler. Så spis endelig løs – især af de danske og solmodne tomater, agurker og peberfrugter.</span>
+</h1>
+  </div>
+    <div class="img">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur, neque sit. Deleniti labore, eum inventore illum porro quam pariatur ipsam sapiente nulla magnam sit repellat, odio tempora tenetur voluptatum dolores.
+    </div>
   </div>
 
 
-  <div class="Container0"></div>
+  <div class="Container0">
+    <div class="text"></div>
+    <div class="img"></div>
+
+  </div>
+
 
   <footer>
     Hello
