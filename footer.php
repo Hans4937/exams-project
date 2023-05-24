1c635add0393a6e6081d76cacc65e1178546f308
tdkr49380 <tdkr49380@edu.ucl.dk>
2023-05-24 23:34:25 +0200

    240523
    Navbar done, index mangler kun import af navbar og footer. Footer mangler lige lidt.

---
+++ footer.php
@@ -0,0 +1,75 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- linking script and stylesheet -->
+  <script src="./scripts.js"></script>
+  <link rel="stylesheet" href="./styles.css"></link>
+</head>
+<body>
+<footer>
+    <div class="Container1">
+       
+        <div class="FooterHeader">
+             <a href="index.php"><img class="Logo" src="./img/Logo.png" alt="sa"></a>
+            <a href="index.php"><h1>Nordic Greens</h1></a>
+        </div>
+       
+        <div class="Main">
+            <div class="FooterColumn">
+            <ul>
+                <li>Assensvej 217, 5250 Odense SV,Danmark</li>
+                <li>CVR-nr. 29 63 45 21</li>
+                <li><a href="#">Linkedin</a></li>
+            </ul>
+            </div>
+
+            <div class="Row">
+                <div class="PedersUdvalgte">
+                    <h4>Peders Udvalgte</h4>
+                    <a href="#">Facebook</a>
+                    <a href="#">Linkedin</a>
+                </div>
+
+                <div class="KatrineAlfreds">
+                    <h4>Katrine & Alfreds</h4>
+                    <a href="#">Facebook</a>
+                    <a href="#">Linkedin</a>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="Main1">
+            <div class="Menu">
+                <div class="Column">
+       
+                    <ul>
+                        <li><a href="#">Produkter</a></li>
+                        <li><a href="#">Brands</a></li>
+                        <li><a href="#">Opskrifter</a></li>
+                        <li><a href="#">Madspild</a></li>
+                        <li><a href="#">Miljø & CSR</a></li>
+                        <li><a href="#">Om os</a></li>
+                        <li><a href="#">Presse</a></li>
+                        <li><a href="#">Jobs</a></li>
+                        <li><a href="#">Erhverv</a></li>
+                        <li><a href="#">Kontakt os</a></li>
+                    </ul>
+                    </div>
+            </div>
+        </div>
+        
+        
+
+                  
+       
+
+    </div>
+  </footer>
+</body>
+</html>
